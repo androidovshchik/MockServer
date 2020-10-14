e1e2e1495d4169be90eaa3c27571f6a0fdbaefb1
vlad <vladkalyuzhnyu@gmail.com>
2020-10-14 09:44:17 +0300

    init

--- app/src/local/kotlin/defpackage/mockserver/remote/ServerMock.kt
+++ app/src/local/kotlin/defpackage/mockserver/remote/ServerMock.kt
@@ -5,6 +5,9 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import defpackage.mockserver.remote.api.Post
 import io.reactivex.Observable
+import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.HttpException
+import retrofit2.Response
 import java.util.concurrent.TimeUnit
 
 class ServerMock(
@@ -14,8 +17,11 @@ class ServerMock(
 
     override fun getPosts(): Observable<List<Post>> {
         return Observable.fromCallable {
+            if ((1..10).random() < 3) {
+                throw HttpException(Response.error<Any>(404, "".toResponseBody()))
+            }
             val json = context.assets.open("posts.json").bufferedReader().use { it.readText() }
             gson.fromJson<List<Post>>(json, TypeToken.getParameterized(List::class.java, Post::class.java).type)
-        }.delay((200..1000).random().toLong(), TimeUnit.MILLISECONDS)
+        }.delaySubscription((200..1000).random().toLong(), TimeUnit.MILLISECONDS)
     }
 }
